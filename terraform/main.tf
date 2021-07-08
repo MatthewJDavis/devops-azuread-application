310c2836135e897863d4cd92c9058f7fe986e58d
MatthewJDavis <matthewdavis111@gmail.com>
2021-07-07 22:08:53 -0400

    move backend to partial conf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,5 @@
 terraform {
   backend "azurerm" {
-    resource_group_name   = "terraform-state"
-    storage_account_name  = "mdterraform"
-    container_name        = "az-devops-state"
-    key                   = "msgraphdirrole.tfstate"
   }
 }
 
